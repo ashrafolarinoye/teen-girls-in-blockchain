16fd246f518724eab51d7e0722c8ccb7aaac4135
ashrafolarinoye <ashrafolarinoye@gmail.com>
2023-04-23 08:02:55 +0100

    bg dark mode

--- index.js
+++ index.js
@@ -8,15 +8,3 @@ if (currentTheme) {
         toggleSwitch.checked = true;
     }
 }
-
-function switchTheme(e) {
-    if (e.target.checked) {
-        document.documentElement.setAttribute('data-theme', 'dark');
-        localStorage.setItem('theme', 'dark');
-    }
-    else {        document.documentElement.setAttribute('data-theme', 'light');
-          localStorage.setItem('theme', 'light');
-    }    
-}
-
-toggleSwitch.addEventListener('change', switchTheme, false);
