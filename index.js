97b31efb0940b6d56706d6c0a07de5bb89216745
ashrafolarinoye <ashrafolarinoye@gmail.com>
2023-08-02 19:35:53 +0100

    scroll js

--- index.js
+++ index.js
@@ -8,3 +8,22 @@ if (currentTheme) {
         toggleSwitch.checked = true;
     }
 }
+
+/**
+ * header & back top btn show when scroll down to 100px
+ */
+
+const header = document.querySelector("[data-header]");
+const backTopBtn = document.querySelector("[data-back-top-btn]");
+
+const headerActive = function () {
+  if (window.scrollY > 80) {
+    header.classList.add("active");
+    backTopBtn.classList.add("active");
+  } else {
+    header.classList.remove("active");
+    backTopBtn.classList.remove("active");
+  }
+}
+
+addEventOnElem(window, "scroll", headerActive);
